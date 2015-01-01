bf0ffe802589e848823c803041d1a9a5f9d49fa6
Boopathi Rajaa <me@boopathi.in>
2015-01-01 19:46:55 +0530

    Adds middleware for CSRF Token, Login etc...

--- main.go
+++ main.go
@@ -3,28 +3,27 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 	"log"
-	"./controllers"
-)
-
-const (
-	ADDRESS = ":3000"
+	"github.com/nobelium/dalalstreet/controllers"
 )
 
 func main() {
-	log.Println("Starting server on: ", ADDRESS)
-
 	r := mux.NewRouter()
 
-	
 	r.HandleFunc("/login", controllers.LoginHandler).Methods("GET")
 	r.HandleFunc("/login", controllers.LoginProcessor).Methods("POST")
 	r.HandleFunc("/logout", controllers.LogoutHandler).Methods("GET", "POST")
-	
 	r.HandleFunc("/", controllers.IndexHandler).Methods("GET")
 
-	http.Handle("/", r)
+	m := new(Middleware)
+	m.UseCSRF()
+	m.UseLogger()
+
+	http.Handle("/", m.Init(r))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./assets/"))))
 
-	http.ListenAndServe(ADDRESS, nil)
+	address := HOST + ":" + strconv.Itoa(PORT)
+	log.Println("Starting server on: ", address)
+	http.ListenAndServe(address, nil)
 }

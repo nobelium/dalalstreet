9368c26f3b619d464d2360ad67366411df3a86e8
nobelium <me@vignesh.info>
2015-01-02 11:24:54 +0530

    Templater function works - needs dictator function

--- controllers/IndexController.go
+++ controllers/IndexController.go
@@ -4,20 +4,14 @@ import (
 	"net/http"
 	"log"
 	"github.com/nobelium/dalalstreet/config"
-	"html/template"
 )
 
 func IndexHandler (res http.ResponseWriter, req *http.Request) {
 	log.Println("Reached IndexHandler")
-	session, _ := config.Store.Get(req, "flash-session")
-	session.Values["store"] = "asdfasdf--"
-	session.Save(req, res)
+	// session := config.GetSession(req)
+	// session.Values["store"] = "asdfasdf--"
+	// session.Save(req, res)
 
 	// http.ServeFile(res, req, "./views/index.html")
-	
-	t := template.Must(template.ParseFiles("views/_base.html", "views/index.html"))
-	err := t.Execute(res, nil)
-	if err != nil {
-		log.Fatalf("template execution: %s", err)
-	}
+	config.Render(res, config.T("index.html"), nil)
 }
--- controllers/SessionController.go
+++ controllers/SessionController.go
@@ -3,18 +3,26 @@ package controllers
 import (
 	"net/http"
 	"log"
-	"fmt"
+	// "fmt"
+	"github.com/nobelium/dalalstreet/config"
 )
 
 func LoginHandler (res http.ResponseWriter, req *http.Request) {
 	log.Println("Reached LoginHandler")
+	config.Render(res, config.T("login.html"), nil)
 }
 
 func AuthHandler (res http.ResponseWriter, req *http.Request) {
 	log.Println("Reached AuthHandler")
+	http.Redirect(res, req, "/", http.StatusFound)
 }
 
 func LogoutHandler (res http.ResponseWriter, req *http.Request) {
 	log.Println("Reached LogoutHandler")
-	fmt.Fprint(res, "Logged out")
+	// fmt.Fprint(res, "Logged out")
+	session := config.GetSession(req)
+	session.AddFlash("You have been Logged out", config.MessageName)
+	session.Save(req, res)
+
+	http.Redirect(res, req, "/", http.StatusFound)
 }

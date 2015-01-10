344d85d751a30b1b3f49aff6b6cf76286ce48b46
nobelium <me@vignesh.info>
2015-01-10 10:19:55 +0530

    Bug fix for logged in check

--- controllers/TradeController.go
+++ controllers/TradeController.go
@@ -13,91 +13,89 @@ func BuyingHandler (res http.ResponseWriter, req *http.Request) {
 	// http.ServeFile(res, req, "./views/index.html")
 	log.Println("Reached TradeHandler")
 	
-	if(models.IsLoggedIn(req)){
+	if(models.IsLoggedIn(req) == false){
 		http.Redirect(res, req, "/login", http.StatusUnauthorized)
-	}
-
-	user := models.GetLoggedInUser(req)
-	
-	stockId, _ := strconv.Atoi(req.FormValue("StockId"))
-	number, _ := strconv.Atoi(req.FormValue("Number"))
-	value, _ := strconv.ParseFloat(req.FormValue("Value"), 64)
-
-	buytrans := &models.Buy{0, stockId, user.Username, number, value}
-	ok, err := buytrans.AddBuyingTrans()
-
-	res.Header().Set("Content-Type", "application/json")
-	if(ok == true) {
-		json_buytrans, _ := json.Marshal(buytrans)
-		fmt.Fprint(res, string(json_buytrans))
 	} else {
-		json_err, _ := json.Marshal(err)
-		fmt.Fprint(res, string(json_err))
-	}
+		user := models.GetLoggedInUser(req)
+	
+		stockId, _ := strconv.Atoi(req.FormValue("StockId"))
+		number, _ := strconv.Atoi(req.FormValue("Number"))
+		value, _ := strconv.ParseFloat(req.FormValue("Value"), 64)
 
+		buytrans := &models.Buy{0, stockId, user.Username, number, value}
+		ok, err := buytrans.AddBuyingTrans()
+
+		res.Header().Set("Content-Type", "application/json")
+		if(ok == true) {
+			json_buytrans, _ := json.Marshal(buytrans)
+			fmt.Fprint(res, string(json_buytrans))
+		} else {
+			json_err, _ := json.Marshal(err)
+			fmt.Fprint(res, string(json_err))
+		}
+	}
 }
 
 func SellingHandler (res http.ResponseWriter, req *http.Request) {
 	// http.ServeFile(res, req, "./views/index.html")
 	log.Println("Reached TradeHandler")
 	
-	if(models.IsLoggedIn(req)){
+	if(models.IsLoggedIn(req) == false){
 		http.Redirect(res, req, "/login", http.StatusUnauthorized)
-	}
-
-	user := models.GetLoggedInUser(req)
-
-	stockId, _ := strconv.Atoi(req.FormValue("StockId"))
-	number, _ := strconv.Atoi(req.FormValue("Number"))
-	value, _ := strconv.ParseFloat(req.FormValue("Value"), 64)
-
-	selltrans := &models.Sell{0, stockId, user.Username, number, value}
-	ok, err := selltrans.AddSellingTrans()
-
-	res.Header().Set("Content-Type", "application/json")
-	if(ok == true) {
-		json_selltrans, _ := json.Marshal(selltrans)
-		fmt.Fprint(res, string(json_selltrans))
 	} else {
-		json_err, _ := json.Marshal(err)
-		fmt.Fprint(res, string(json_err))
-	}
+		user := models.GetLoggedInUser(req)
 
+		stockId, _ := strconv.Atoi(req.FormValue("StockId"))
+		number, _ := strconv.Atoi(req.FormValue("Number"))
+		value, _ := strconv.ParseFloat(req.FormValue("Value"), 64)
+
+		selltrans := &models.Sell{0, stockId, user.Username, number, value}
+		ok, err := selltrans.AddSellingTrans()
+
+		res.Header().Set("Content-Type", "application/json")
+		if(ok == true) {
+			json_selltrans, _ := json.Marshal(selltrans)
+			fmt.Fprint(res, string(json_selltrans))
+		} else {
+			json_err, _ := json.Marshal(err)
+			fmt.Fprint(res, string(json_err))
+		}
+	}
 }
 
 func BuyFromExchangeHandler (res http.ResponseWriter, req *http.Request) {
 	// http.ServeFile(res, req, "./views/index.html")
 	log.Println("Reached TradeHandler")
 	
-	if(models.IsLoggedIn(req)){
+	if(models.IsLoggedIn(req) == false){
 		http.Redirect(res, req, "/login", http.StatusUnauthorized)
+	} else {
+		user := models.GetLoggedInUser(req)
+		stockId, number := req.FormValue("StockId"), req.FormValue("Number")
+		fmt.Fprint(res, stockId, number, user)
 	}
-
-	user := models.GetLoggedInUser(req)
-	stockId, number := req.FormValue("StockId"), req.FormValue("Number")
-	fmt.Fprint(res, stockId, number, user)
 }
 
 func CancelBuyingOrderHandler (res http.ResponseWriter, req *http.Request) {
 	// http.ServeFile(res, req, "./views/index.html")
 	log.Println("Reached TradeHandler")
 	
-	if(models.IsLoggedIn(req)){
+	if(models.IsLoggedIn(req) == false){
 		http.Redirect(res, req, "/login", http.StatusUnauthorized)
+	} else {
+		user := models.GetLoggedInUser(req)
+		fmt.Fprint(res, user)
 	}
-
-	user := models.GetLoggedInUser(req)
-	fmt.Fprint(res, user)
 }
 
 func CancelSellingOrderHandler (res http.ResponseWriter, req *http.Request) {
 	// http.ServeFile(res, req, "./views/index.html")
 	log.Println("Reached TradeHandler")
 	
-	if(models.IsLoggedIn(req)){
+	if(models.IsLoggedIn(req) == false){
 		http.Redirect(res, req, "/login", http.StatusUnauthorized)
+	} else {
+		user := models.GetLoggedInUser(req)
+		fmt.Fprint(res, user)
 	}
-
-	user := models.GetLoggedInUser(req)
-	fmt.Fprint(res, user)
 }
